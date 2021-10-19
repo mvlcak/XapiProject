b52fdf950e52ed11e9d0a80ceac13358ee770891
kroko-kroko <m.vlcak@gmail.com>
2021-10-19 16:53:54 +0200

    added detail person and activity

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /polls/
+    # ex: /app/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /activity/5/
+    path('<int:activity_id>/', views.detailActivity, name='detail'),
+     # ex: /person/5/
+    path('<int:person_id>/', views.detailPerson, name='detail'),
     
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -6,7 +6,9 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def detail(request, person_id):
-    return HttpResponse("You're looking at person %s." % person_id)
+def detailPerson(request, person_id):
+    return HttpResponse("You're looking at person %s." %person_id)
 
+def detailActivity(request, activity_id):
+    return HttpResponse("You're looking at activity %s." %activity_id)
     
