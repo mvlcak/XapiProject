01d4adce30d18b565b0b7379f4d9e21d0dd46a86
kroko-kroko <m.vlcak@gmail.com>
2021-10-19 12:27:08 +0200

    initial commit

---
+++ project/app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.person_name
+    
+
+class Activity(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    actor = models.CharField(max_length=200)
+    verb = models.CharField(max_length=200)
+    object = models.CharField(max_length=200)   
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.actor +" "+self.verb+" "+self.object
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
