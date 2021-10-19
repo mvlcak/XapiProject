5f7c894e496e489fc89335d98b5b537daaa17a22
kroko-kroko <m.vlcak@gmail.com>
2021-10-19 12:30:53 +0200

    showing person

--- project/app/views.py
+++ project/app/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, person_id):
+    return HttpResponse("You're looking at person %s." % person_id)
+
+    
