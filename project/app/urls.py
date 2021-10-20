3bbb029f0f0817d403a08b0bebde99653a38f7db
kroko-kroko <m.vlcak@gmail.com>
2021-10-20 11:05:50 +0200

    adding registration

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     # ex: /app/
     path('', views.index, name='index'),
-    # ex: /activity/5/
-    path('<int:activity_id>/', views.detailActivity, name='detail'),
-     # ex: /person/5/
-    path('<int:person_id>/', views.detailPerson, name='detail'),
+    # ex: /activity/id/
+    path('activity/<int:activity_id>/', views.detailActivity, name='detailActivity'),
+     # ex: /person/id/
+    path('person/<int:person_id>/', views.detailPerson, name='detailPerson'),
+    # ex: /register/
+    path('register', views.registerPage, name='registerPage'),
     
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+from .models import *
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from .forms import  CreateUserForm
 
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'app/register.html',context)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'app/index.html')
 
 def detailPerson(request, person_id):
-    return HttpResponse("You're looking at person %s." %person_id)
+    person = get_object_or_404(Person, pk=person_id)
+    return render(request, 'app/detailPerson.html', {'person': person})
 
 def detailActivity(request, activity_id):
     return HttpResponse("You're looking at activity %s." %activity_id)
