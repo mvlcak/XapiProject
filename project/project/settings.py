5c33684928fa0c5e74d8ad4cc991efc8230312b7
kroko-kroko <m.vlcak@gmail.com>
2021-11-20 14:25:55 +0100

    added cronjob

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_crontab',
      'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -130,4 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['*']
+
 
